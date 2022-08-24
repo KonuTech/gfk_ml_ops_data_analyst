619ae262cca2c0bd30ca20bf1db837b952f362b2
KonuTech <borowiec.k@gmail.com>
2022-08-24 20:01:10 +0200

    Added pandas-profiling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Model Bias Measurements
+
+> Amazon AI Fairness and Explainability Whitepaper
+
 [SOURCE](https://pages.awscloud.com/rs/112-TZM-766/images/Amazon.AI.Fairness.and.Explainability.Whitepaper.pdf)
--- scripts/python/get_metrics.py
+++ scripts/python/get_metrics.py
@@ -29,7 +29,7 @@ def get_PPL(data_frame, column_to_group_by, column_to_count):
     .sort_values(by=column_to_group_by, ascending=False) \
     .set_index(column_to_group_by)
 
-    positive_counts = counts_grouped.loc[counts_grouped[column_to_count] == '1']
+    positive_counts = counts_grouped.loc[counts_grouped[column_to_count] == 1]
     df = pd.merge(positive_counts, counts_total, left_index=True, right_index=True)
     df['PPL'] = df['count'] / df['count_total']
     
@@ -78,7 +78,7 @@ def get_CA(data_frame, column_to_group_by, column_to_count_acctuals, column_to_c
     .sort_values(by=column_to_group_by, ascending=False) \
     .set_index(column_to_group_by)
     
-    positive_counts_grouped_acctuals = counts_grouped_acctuals.loc[counts_grouped_acctuals[column_to_count_acctuals] == '1']
+    positive_counts_grouped_acctuals = counts_grouped_acctuals.loc[counts_grouped_acctuals[column_to_count_acctuals] == 1]
     
     
     counts_grouped_predictions = data_frame.groupby([column_to_group_by, column_to_count_predictions]) \
@@ -88,7 +88,7 @@ def get_CA(data_frame, column_to_group_by, column_to_count_acctuals, column_to_c
     .sort_values(by=column_to_group_by, ascending=False) \
     .set_index(column_to_group_by)
 
-    positive_counts_grouped_predictions= counts_grouped_predictions.loc[counts_grouped_predictions[column_to_count_predictions] == '1']
+    positive_counts_grouped_predictions= counts_grouped_predictions.loc[counts_grouped_predictions[column_to_count_predictions] == 1]
 
 
     df = pd.merge(positive_counts_grouped_acctuals, positive_counts_grouped_predictions, left_index=True, right_index=True)
@@ -140,7 +140,7 @@ def get_CR(data_frame, column_to_group_by, column_to_count_acctuals, column_to_c
     .sort_values(by=column_to_group_by, ascending=False) \
     .set_index(column_to_group_by)
     
-    negative_counts_grouped_acctuals = counts_grouped_acctuals.loc[counts_grouped_acctuals[column_to_count_acctuals] == '0']
+    negative_counts_grouped_acctuals = counts_grouped_acctuals.loc[counts_grouped_acctuals[column_to_count_acctuals] == 0]
     
     
     counts_grouped_predictions = data_frame.groupby([column_to_group_by, column_to_count_predictions]) \
@@ -150,7 +150,7 @@ def get_CR(data_frame, column_to_group_by, column_to_count_acctuals, column_to_c
     .sort_values(by=column_to_group_by, ascending=False) \
     .set_index(column_to_group_by)
 
-    negative_counts_grouped_predictions= counts_grouped_predictions.loc[counts_grouped_predictions[column_to_count_predictions] == '0']
+    negative_counts_grouped_predictions= counts_grouped_predictions.loc[counts_grouped_predictions[column_to_count_predictions] == 0]
 
 
     df = pd.merge(negative_counts_grouped_acctuals, negative_counts_grouped_predictions, left_index=True, right_index=True)
@@ -176,3 +176,18 @@ def get_DCR(data_frame, column_to_group_by, column_to_count_acctuals, column_to_
         cr.append(i[1])
     
     return sorted(set([i - j for i in cr for j in cr if i != j]))
+
+
+def get_class_cm(data_frame, acctuals, predictions, column_to_group_by):
+    
+    data = pd.DataFrame(columns=['Class', 'TN', 'FN', 'FP', 'TP'])
+    
+    for group in set(data_frame[column_to_group_by]):
+        sample = data_frame.loc[data_frame[column_to_group_by] == group]
+    
+        cm = confusion_matrix(sample[acctuals], sample[predictions].notnull())
+        TN, FN, FP, TP = cm[0][0], cm[1][0], cm[0][1], cm[1][1]
+        
+        data = data.append({'Class': group, 'TN': TN, 'FN': FN, 'FP': FP, 'TP': TP}, ignore_index=True)
+        
+    return data
