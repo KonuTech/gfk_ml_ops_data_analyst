0fbcf0c29a52c1e3b5bee5b1a6187536a2f6361e
KonuTech <borowiec.k@gmail.com>
2022-08-26 08:52:36 +0200

    Added pandas-profiling

--- scripts/python/get_charts_01.py
+++ scripts/python/get_charts_01.py
@@ -19,16 +19,19 @@ def get_monthly_stability_chart(col, data_frame, date_column, column_to_group_by
 
     data_frame['month_year'] = data_frame[date_column].dt.to_period('M')
     total = data_frame.groupby([data_frame['month_year'], column_to_group_by])[column_to_count].count().reset_index()
-    total['group'] = total['month_year'].astype(str) + " " + total[column_to_group_by].astype(str)
 
     rate = data_frame[data_frame[column_to_count] == 1].groupby([data_frame['month_year'], column_to_group_by])[column_to_count].sum().reset_index()
-    rate['group'] = rate['month_year'].astype(str) + " " + rate[column_to_group_by].astype(str)
+    rate_longest['rate'] = [(i / j * 100) if (i not in (None, 0) and j not in (None, 0)) else 0 for i, j in itertools.zip_longest(rate[column_to_count], total[column_to_count])]
+    rate_longest['group'] = total['month_year'].astype(str) + " " + total[column_to_group_by].astype(str)
+    rate_longest[column_to_count] = rate[column_to_count]
+    #     print(rate, '\n', "DF SHAPE: ", rate.shape)
 
+    total['total_rate'] = [(i / j * 100) if (i not in (None, 0) and j not in (None, 0)) else 100 for i, j in itertools.zip_longest(total[column_to_count], total[column_to_count])]
+    total['group'] = total['month_year'].astype(str) + " " + total[column_to_group_by].astype(str)
+    #     print(total, '\n', "DF SHAPE: ", total.shape)
 
-    output = pd.merge(rate[['group', column_to_count]], total, how='right', left_on='group', right_on='group')
-    output.rename(columns={column_to_count + '_x': "predict_1", column_to_count + '_y': "predict_total"}, inplace=True)
-    output['rate'] = output['predict_1'] / output['predict_total']
-    output['rate_total'] = 1.0
+    # output = pd.merge(total, rate_longest, how='left', left_on='group', right_on='group')
+    output = pd.merge(total, rate_longest, left_on='group', right_on='group')
     print(output, '\n', "DF SHAPE: ", output.shape)
 
     with open('out.txt', 'a') as f:
@@ -47,7 +50,7 @@ def get_monthly_stability_chart(col, data_frame, date_column, column_to_group_by
         print("PRODUCED A CHART OF " + str(column_to_count) + " MONTHLY STABILITY FOR: ", str(col), " VARIABLE CLASS: ", group)
         bar1 = sns.barplot(
             x='month_year',
-            y="rate_total",
+            y="total_rate",
             data=output[output[column_to_group_by] == group],
             color='darkblue',
             # alpha=0.5
