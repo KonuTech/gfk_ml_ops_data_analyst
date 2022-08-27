0f5a6adb60a02a7455ed5a7c280043fb8de8b304
KonuTech <borowiec.k@gmail.com>
2022-08-27 14:05:58 +0200

    Added Evidently reports

--- get_all_visualizations.py
+++ get_all_visualizations.py
@@ -4,33 +4,32 @@ import os
 import logging
 from scripts.python.read_csv import read_input_csv
 from scripts.python.get_reports import *
+from scripts.python.get_charts import get_monthly_stability_chart, get_weekly_stability_chart
 
 
-logging.basicConfig(filename='logs/logs.log', filemode='a', format='%(asctime)s - %(message)s', \
+logging.basicConfig(filename='logs/logs.log', filemode='a', format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
-logging.info('GLOBAL VARIABLES SET UP')
-
 
 # SET GLOBAL VARIABLES
-INPUT_PATH="input"
-OUTPUT_PATH="output"
-CONFIG_PATH="config"
+INPUT_PATH = "input"
+OUTPUT_PATH = "output"
+CONFIG_PATH = "config"
 
-INPUT_DATA="test_data2"
-INPUT_DATA_CONFIG="input_config"
-OUTPUT_DATA="output"
+INPUT_DATA = "test_data2"
+INPUT_DATA_CONFIG = "input_config"
+OUTPUT_DATA = "output"
 
-INPUT_EXTENSION="csv"
-INPUT_CONFIG_EXTENSION="json"
-OUTPUT_EXTENSION="csv"
+INPUT_EXTENSION = "csv"
+INPUT_CONFIG_EXTENSION = "json"
+OUTPUT_EXTENSION = "csv"
 
-INPUT_FILE=f"{INPUT_DATA}.{INPUT_EXTENSION}"
-INPUT_CONFIG_FILE=f"{INPUT_DATA_CONFIG}.{INPUT_CONFIG_EXTENSION}"
-OUTPUT_FILE=f"{OUTPUT_DATA}.{OUTPUT_EXTENSION}"
+INPUT_FILE = f"{INPUT_DATA}.{INPUT_EXTENSION}"
+INPUT_CONFIG_FILE = f"{INPUT_DATA_CONFIG}.{INPUT_CONFIG_EXTENSION}"
+OUTPUT_FILE = f"{OUTPUT_DATA}.{OUTPUT_EXTENSION}"
 
-INPUT_ABS_APTH=os.path.abspath(os.path.join(INPUT_PATH, INPUT_FILE))
-INPUT_FILE_CONFIG=os.path.abspath(os.path.join(CONFIG_PATH, INPUT_CONFIG_FILE))
-OUTPUT_ABS_APTH=os.path.abspath(os.path.join(OUTPUT_PATH, OUTPUT_FILE))
+INPUT_ABS_PATH = os.path.abspath(os.path.join(INPUT_PATH, INPUT_FILE))
+INPUT_FILE_CONFIG = os.path.abspath(os.path.join(CONFIG_PATH, INPUT_CONFIG_FILE))
+OUTPUT_ABS_PATH = os.path.abspath(os.path.join(OUTPUT_PATH, OUTPUT_FILE))
 
 
 def main():
@@ -38,106 +37,154 @@ def main():
     :return:
     """
 
-
-    logging.info('MAIN FUNCTION STARTED')
-
+    logging.info('####### MAIN FUNCTION STARTED #######')
 
     # LOAD CSV INPUT USING JSON CONFIG
-    df, CONFIG = read_input_csv(INPUT_ABS_APTH, INPUT_FILE_CONFIG)
+    df, config = read_input_csv(INPUT_ABS_PATH, INPUT_FILE_CONFIG)
 
-
-    logging.info('CONFIG JSON FOR CSV LOADED')
+    logging.info('config JSON FOR CSV LOADED')
     logging.info('CSV LOADED')
 
-
-    # TARGET DRIFT - produce global report
-    get_target_drift_report(
-        data_frame=df,
-        target=CONFIG['MODEL']['TARGET'],
-        prediction=CONFIG['MODEL']['PREDICTION'],
-        datetime=CONFIG['MODEL']['DATETIME'],
-        categorical_fatures=CONFIG['INPUTS']['CATEGORICAL_FEATURES'],
-        columns_to_exclude=CONFIG['OUTPUTS']['COLUMNS_TO_EXCLUDE'],
-        breaking_point_dt=CONFIG['OUTPUTS']['BREAKING_POINT_DT']
-    )
-
-
-    logging.info('TARGET DRIFT - produced global report')
-
-
-    # TARGET DRIFT - produce weekly reports
-    get_target_drift_report_weekly(
-        data_frame=df,
-        target=CONFIG['MODEL']['TARGET'],
-        prediction=CONFIG['MODEL']['PREDICTION'],
-        datetime=CONFIG['MODEL']['DATETIME'],
-        categorical_fatures=CONFIG['INPUTS']['CATEGORICAL_FEATURES'],
-        columns_to_exclude=CONFIG['OUTPUTS']['COLUMNS_TO_EXCLUDE']
-    )
-
-
-    logging.info('TARGET DRIFT - produced weekly reports')
-
-
-    # DATA DRIFT - produce global report
-    get_data_drift_report(
-        data_frame=df,
-        target=CONFIG['MODEL']['TARGET'],
-        prediction=CONFIG['MODEL']['PREDICTION'],
-        datetime=CONFIG['MODEL']['DATETIME'],
-        categorical_fatures=CONFIG['INPUTS']['CATEGORICAL_FEATURES'],
-        columns_to_exclude=CONFIG['OUTPUTS']['COLUMNS_TO_EXCLUDE'],
-        breaking_point_dt=CONFIG['OUTPUTS']['BREAKING_POINT_DT']
-    )
-
-
-    logging.info('DATA DRIFT - produced global report')
-
-
-    # DATA DRIFT - produce weekly reports
-    get_data_drift_report_weekly(
-        data_frame=df,
-        target=CONFIG['MODEL']['TARGET'],
-        prediction=CONFIG['MODEL']['PREDICTION'],
-        datetime=CONFIG['MODEL']['DATETIME'],
-        categorical_fatures=CONFIG['INPUTS']['CATEGORICAL_FEATURES'],
-        columns_to_exclude=CONFIG['OUTPUTS']['COLUMNS_TO_EXCLUDE']
-    )
-
-
-    logging.info('DATA DRIFT - produced weekly reports')
-
-
-    # CLASSIFICATION PERFORMANCE - produce global report
-    get_classification_performance_report(
-        data_frame=df,
-        target=CONFIG['MODEL']['TARGET'],
-        prediction=CONFIG['MODEL']['PREDICTION'],
-        datetime=CONFIG['MODEL']['DATETIME'],
-        categorical_fatures=CONFIG['INPUTS']['CATEGORICAL_FEATURES'],
-        columns_to_exclude=CONFIG['OUTPUTS']['COLUMNS_TO_EXCLUDE'],
-        breaking_point_dt=CONFIG['OUTPUTS']['BREAKING_POINT_DT']
-    )
-
-
-    logging.info('CLASSIFICATION PERFORMANCE - produced global report')
-
-
-    # CLASSIFICATION PERFORMANCE - produce weekly reports
-    get_classification_performance_report_weekly(
-        data_frame=df,
-        target=CONFIG['MODEL']['TARGET'],
-        prediction=CONFIG['MODEL']['PREDICTION'],
-        datetime=CONFIG['MODEL']['DATETIME'],
-        categorical_fatures=CONFIG['INPUTS']['CATEGORICAL_FEATURES'],
-        columns_to_exclude=CONFIG['OUTPUTS']['COLUMNS_TO_EXCLUDE']
-    )
-
+    # # TARGET DRIFT - produce global report
+    # get_target_drift_report(
+    #     data_frame=df,
+    #     target=config['MODEL']['TARGET'],
+    #     prediction=config['MODEL']['PREDICTION'],
+    #     datetime=config['MODEL']['DATETIME'],
+    #     categorical_fatures=config['INPUTS']['CATEGORICAL_FEATURES'],
+    #     columns_to_exclude=config['OUTPUTS']['COLUMNS_TO_EXCLUDE'],
+    #     breaking_point_dt=config['OUTPUTS']['BREAKING_POINT_DT']
+    # )
+    #
+    # logging.info('TARGET DRIFT - produced global report')
+    #
+    # # TARGET DRIFT - produce weekly reports
+    # get_target_drift_report_weekly(
+    #     data_frame=df,
+    #     target=config['MODEL']['TARGET'],
+    #     prediction=config['MODEL']['PREDICTION'],
+    #     datetime=config['MODEL']['DATETIME'],
+    #     categorical_fatures=config['INPUTS']['CATEGORICAL_FEATURES'],
+    #     columns_to_exclude=config['OUTPUTS']['COLUMNS_TO_EXCLUDE']
+    # )
+    #
+    # logging.info('TARGET DRIFT - produced weekly reports')
+    #
+    # # DATA DRIFT - produce global report
+    # get_data_drift_report(
+    #     data_frame=df,
+    #     target=config['MODEL']['TARGET'],
+    #     prediction=config['MODEL']['PREDICTION'],
+    #     datetime=config['MODEL']['DATETIME'],
+    #     categorical_fatures=config['INPUTS']['CATEGORICAL_FEATURES'],
+    #     columns_to_exclude=config['OUTPUTS']['COLUMNS_TO_EXCLUDE'],
+    #     breaking_point_dt=config['OUTPUTS']['BREAKING_POINT_DT']
+    # )
+    #
+    # logging.info('DATA DRIFT - produced global report')
+    #
+    # # DATA DRIFT - produce weekly reports
+    # get_data_drift_report_weekly(
+    #     data_frame=df,
+    #     target=config['MODEL']['TARGET'],
+    #     prediction=config['MODEL']['PREDICTION'],
+    #     datetime=config['MODEL']['DATETIME'],
+    #     categorical_fatures=config['INPUTS']['CATEGORICAL_FEATURES'],
+    #     columns_to_exclude=config['OUTPUTS']['COLUMNS_TO_EXCLUDE']
+    # )
+    #
+    # logging.info('DATA DRIFT - produced weekly reports')
+    #
+    # # CLASSIFICATION PERFORMANCE - produce global report
+    # get_classification_performance_report(
+    #     data_frame=df,
+    #     target=config['MODEL']['TARGET'],
+    #     prediction=config['MODEL']['PREDICTION'],
+    #     datetime=config['MODEL']['DATETIME'],
+    #     categorical_fatures=config['INPUTS']['CATEGORICAL_FEATURES'],
+    #     columns_to_exclude=config['OUTPUTS']['COLUMNS_TO_EXCLUDE'],
+    #     breaking_point_dt=config['OUTPUTS']['BREAKING_POINT_DT']
+    # )
+    #
+    # logging.info('CLASSIFICATION PERFORMANCE - produced global report')
+    #
+    # # CLASSIFICATION PERFORMANCE - produce weekly reports
+    # get_classification_performance_report_weekly(
+    #     data_frame=df,
+    #     target=config['MODEL']['TARGET'],
+    #     prediction=config['MODEL']['PREDICTION'],
+    #     datetime=config['MODEL']['DATETIME'],
+    #     categorical_fatures=config['INPUTS']['CATEGORICAL_FEATURES'],
+    #     columns_to_exclude=config['OUTPUTS']['COLUMNS_TO_EXCLUDE']
+    # )
 
     logging.info('CLASSIFICATION PERFORMANCE - produced weekly reports')
-    logging.info('MAIN FUNCTION DONE')
+
+    # PREDICT AUTOMATCH MONTHLY STABILITY - produce charts per each class for every variable by month
+    get_monthly_stability_chart(
+        data_frame=df,
+        date_column="translated_when",
+        column_to_count='predict_automatch',
+        columns_to_exclude=[
+            'period_end_date',
+            'translated_when',
+            'month_year',
+            'predict_automatch',
+            'class_acctual'
+        ]
+    )
+
+    logging.info('PREDICT AUTOMATCH MONTHLY STABILITY - produced charts for all classes of variables by month')
+
+    # CLASS ACCTUAL MONTHLY STABILITY - produce charts per each class for every variable by month
+    get_monthly_stability_chart(
+        data_frame=df,
+        date_column="translated_when",
+        column_to_count='class_acctual',
+        columns_to_exclude=[
+            'period_end_date',
+            'translated_when',
+            'month_year',
+            'predict_automatch',
+            'class_acctual'
+        ]
+    )
+
+    logging.info('CLASS ACCTUAL MONTHLY STABILITY - produced charts for all classes of variables by month')
+
+    # PREDICT AUTOMATCH WEEKLY STABILITY - produce charts per each class for every variable by week
+    get_weekly_stability_chart(
+        data_frame=df,
+        date_column="translated_when",
+        column_to_count='predict_automatch',
+        columns_to_exclude=[
+            'period_end_date',
+            'translated_when',
+            'month_year',
+            'predict_automatch',
+            'class_acctual'
+        ]
+    )
+
+    logging.info('PREDICT AUTOMATCH WEEKLY STABILITY - produced charts for all classes of variables by week')
+
+    # CLASS ACCTUAL WEEKLY STABILITY - produce charts per each class for every variable by week
+    get_weekly_stability_chart(
+        data_frame=df,
+        date_column="translated_when",
+        column_to_count='class_acctual',
+        columns_to_exclude=[
+            'period_end_date',
+            'translated_when',
+            'month_year',
+            'predict_automatch',
+            'class_acctual'
+        ]
+    )
+
+    logging.info('CLASS ACCTUAL WEEKLY STABILITY - produced charts for all classes of variables by month')
+    logging.info('####### MAIN FUNCTION DONE #######')
 
 
 if __name__ == "__main__":
     main()
-
--- scripts/python/get_charts.py
+++ scripts/python/get_charts.py
@@ -1,63 +1,142 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns
 import itertools
 
+matplotlib.use("Agg")
 
-def get_monthly_stability_chart(col, data_frame, date_column, column_to_group_by, column_to_count):
+
+def get_monthly_stability_chart(data_frame, date_column, column_to_count, columns_to_exclude):
     """
-    :param col:
     :param data_frame:
     :param date_column:
-    :param column_to_group_by:
     :param column_to_count:
+    :param columns_to_exclude:
     :return:
     """
     from contextlib import redirect_stdout
 
     data_frame['month_year'] = data_frame[date_column].dt.to_period('M')
 
-    total = data_frame.groupby([data_frame['month_year'], column_to_group_by])[column_to_count].count().reset_index()
-    total['group'] = total['month_year'].astype(str) + " " + total[column_to_group_by].astype(str)
+    for column in data_frame.columns:
+        if column not in set(columns_to_exclude):
 
-    rate = data_frame[data_frame[column_to_count] == 1].groupby([data_frame['month_year'], column_to_group_by])[column_to_count].sum().reset_index()
-    rate['group'] = rate['month_year'].astype(str) + " " + rate[column_to_group_by].astype(str)
+            print(column)
 
-    output = pd.merge(rate[['group', column_to_count]], total, how='right', left_on='group', right_on='group')
-    output.rename(columns={column_to_count + '_x': "predict_1", column_to_count + '_y': "predict_total"}, inplace=True)
-    output['rate'] = output['predict_1'] / output['predict_total']
-    output['rate_total'] = 1.0
-    # print(output, '\n', "DF SHAPE: ", output.shape)
+            total = data_frame.groupby([data_frame['month_year'], column])[column_to_count].count().reset_index()
+            total['group'] = total['month_year'].astype(str) + " " + total[column].astype(str)
 
-    with open('output/rates_output.txt', 'w') as f:
-        with redirect_stdout(f):
-            print(output.to_string())
+            rate = data_frame[data_frame[column_to_count] == 1].groupby([data_frame['month_year'], column])[column_to_count].sum().reset_index()
+            rate['group'] = rate['month_year'].astype(str) + " " + rate[column].astype(str)
 
-    for group in set(output[column_to_group_by]):
+            output = pd.merge(rate[['group', column_to_count]], total, how='right', left_on='group', right_on='group')
+            output.rename(columns={column_to_count + '_x': "predict_1", column_to_count + '_y': "predict_total"}, inplace=True)
+            output['rate'] = output['predict_1'] / output['predict_total']
+            output['rate_total'] = 1.0
+            # print(output, '\n', "DF SHAPE: ", output.shape)
 
-        plt.figure(figsize=(10, 2))
+            with open('logs/rates_output.txt', 'w') as f:
+                with redirect_stdout(f):
+                    print(output.to_string())
 
-        # legend
-        top_bar = mpatches.Patch(color='darkblue', label='0')
-        bottom_bar = mpatches.Patch(color='lightblue', label='1')
-        plt.legend(handles=[top_bar, bottom_bar])
+            for group in set(output[column]):
 
-        print("PRODUCED A CHART OF " + str(column_to_count) + " MONTHLY STABILITY FOR: ", str(col), " VARIABLE CLASS: ", group)
-        bar1 = sns.barplot(
-            x='month_year',
-            y="rate_total",
-            data=output[output[column_to_group_by] == group],
-            color='darkblue',
-            # alpha=0.5
-        )
+                plt.figure(figsize=(10, 2))
 
-        bar2 = sns.barplot(
-            x='month_year',
-            y="rate",
-            data=output[output[column_to_group_by] == group],
-            color='lightblue',
-            alpha=0.5
-        )
+                # legend
+                top_bar = mpatches.Patch(color='darkblue', label='0')
+                bottom_bar = mpatches.Patch(color='lightblue', label='1')
+                plt.legend(handles=[top_bar, bottom_bar])
 
-        plt.savefig("output/charts/monthly_stability/" + str(column_to_count) + "/" + str(col) + "/" + "CLASS_" + str(group) + "_monthly_stability_grouped" + '.jpg')
+                try:
+                    sns.barplot(
+                        x='month_year',
+                        y="rate_total",
+                        data=output[output[column] == group],
+                        color='darkblue',
+                        # alpha=0.5
+                    )
+                    # plt.clf()
+
+                    sns.barplot(
+                        x='month_year',
+                        y="rate",
+                        data=output[output[column] == group],
+                        color='lightblue',
+                        alpha=0.5
+                    )
+                    # plt.clf()
+
+                    plt.savefig("output/charts/monthly_stability/" + str(column_to_count) + "/" + str(column) + "/" + "CLASS_" + str(group) + "_monthly_stability_grouped" + '.jpg')
+                    plt.clf()
+                    print("PRODUCED A CHART OF " + str(column_to_count) + " MONTHLY STABILITY FOR: ", str(column), " VARIABLE CLASS: ", group)
+
+                except Exception:
+                    pass
+
+
+def get_weekly_stability_chart(data_frame, date_column, column_to_count, columns_to_exclude):
+    """
+    :param data_frame:
+    :param date_column:
+    :param column_to_count:
+    :param columns_to_exclude:
+    :return:
+    """
+    from contextlib import redirect_stdout
+
+    for column in data_frame.columns:
+        if column not in set(columns_to_exclude):
+
+            data_frame['week'] = data_frame[date_column].dt.to_period('W')
+
+            total = data_frame.groupby([data_frame['week'], column])[column_to_count].count().reset_index()
+            total['group'] = total['week'].astype(str) + " " + total[column].astype(str)
+
+            rate = data_frame[data_frame[column_to_count] == 1].groupby([data_frame['week'], column])[column_to_count].sum().reset_index()
+            rate['group'] = rate['week'].astype(str) + " " + rate[column].astype(str)
+
+            output = pd.merge(rate[['group', column_to_count]], total, how='right', left_on='group', right_on='group')
+            output.rename(columns={column_to_count + '_x': "predict_1", column_to_count + '_y': "predict_total"}, inplace=True)
+            output['rate'] = output['predict_1'] / output['predict_total']
+            output['rate_total'] = 1.0
+            # print(output, '\n', "DF SHAPE: ", output.shape)
+
+            with open('logs/rates_output.txt', 'w') as f:
+                with redirect_stdout(f):
+                    print(output.to_string())
+
+            for group in set(output[column]):
+
+                plt.figure(figsize=(10, 2))
+
+                # legend
+                top_bar = mpatches.Patch(color='darkblue', label='0')
+                bottom_bar = mpatches.Patch(color='lightblue', label='1')
+                plt.legend(handles=[top_bar, bottom_bar])
+
+                try:
+                    sns.barplot(
+                        x='week',
+                        y="rate_total",
+                        data=output[output[column] == group],
+                        color='darkblue',
+                        # alpha=0.5
+                    )
+
+                    sns.barplot(
+                        x='week',
+                        y="rate",
+                        data=output[output[column] == group],
+                        color='lightblue',
+                        alpha=0.5
+                    )
+
+                    plt.savefig("output/charts/weekly_stability/" + str(column_to_count) + "/" + str(column) + "/" + "CLASS_" + str(group) + "_weekly_stability_grouped" + '.jpg')
+                    plt.clf()
+                    print("PRODUCED A CHART OF " + str(column_to_count) + " WEEKLY STABILITY FOR: ", str(column), " VARIABLE CLASS: ", group)
+
+                except Exception:
+                    pass
